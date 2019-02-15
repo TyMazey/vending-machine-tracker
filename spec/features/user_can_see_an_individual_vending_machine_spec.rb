9e1ccdc8a41be6354a5e95311613bfca0a913c7b
Ty Mazey <tymazey@gmail.com>
2019-02-15 10:30:51 -0700

    user story 1

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -12,7 +12,13 @@ feature 'When a user visits a vending machine show page' do
 
   it 'shows all the snacks inside the machine and the price' do
     owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = Snack.create(name: 'Cheetos', price: 1.5)
+    snack_2 = Snack.create(name: 'Jerky', price: 3.5)
+    dons  = owner.machines.create(location: "Don's Mixed Drinks", snacks: [snack_1, snack_2])
 
+    visit machine_path(dons)
+
+    expect(page).to have_content('Cheetos: $1.5')
+    expect(page).to have_content('Jerky: $3.5')
   end
 end
